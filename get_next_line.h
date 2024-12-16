385732589e42bc404132570abfe8d7b8948951a9
David Martinez Gallego <davidma2@c2r12s4.42madrid.com>
2024-12-16 18:12:59 +0100

    B4 Send

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: davidma2 <davidma2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 10:13:20 by davidma2          #+#    #+#             */
-/*   Updated: 2024/12/16 17:45:48 by davidma2         ###   ########.fr       */
+/*   Updated: 2024/12/16 18:06:55 by davidma2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 # define GET_NEXT_LINE_H
 
 # include <stddef.h>
-# include <stdlib.h>
-# include <unistd.h>
-#   include <sys/fcntl.h>
 # include <stdio.h>
+# include <stdlib.h>
+# include <sys/fcntl.h>
+# include <unistd.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 10
